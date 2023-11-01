b2a6afc52e41c74e6dba8f349073f94ae27a3dde
JeromeNL <jw.kwetters@student.avans.nl>
2023-11-01 15:45:25 +0100

    Initial commit

---
+++ app/src/main/java/nl/joramkwetters/basiccalculatorapp/CalculatorState.kt
@@ -0,0 +1,7 @@
+package nl.joramkwetters.basiccalculatorapp
+
+data class CalculatorState (
+    val number1: String = "",
+    val number2: String = "",
+    val operation: CalculatorOperation? = null
+)
